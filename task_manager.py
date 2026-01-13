0010449ed43bb4692b05ba7affcd5e7c351169fb
KTK <117257192+peter1812-byte@users.noreply.github.com>
2026-01-13 22:14:42 +0700

    add basic task manager class

---
+++ task_manager.py
@@ -0,0 +1,9 @@
+class TaskManager:
+    def __init__(self):
+        self.tasks = []
+
+    def add_task(self, task):
+        self.tasks.append(task)
+
+    def get_tasks(self):
+        return self.tasks
